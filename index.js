20319eadaa3a22a0774c275e649d83050fbab18b
gabri32 <88991735+gabri32@users.noreply.github.com>
2023-08-26 22:02:15 -0500

    cambios pasword

--- index.js
+++ index.js
@@ -3,23 +3,22 @@ let button = document.getElementById('btn-enviar');// busca el boton enviar
 button.addEventListener('click', function () {
     //alert('Datos enviados')
     let nombreus = document.getElementById('nombreus');
-    let correous = document.getElementById('correous');
-    if (nombreus.value == '' || correous.value == '') {
+    let pasword = document.getElementById('pasword');
+    if (nombreus.value == '' || pasword.value == '') {
         alert("El campo del nombre o el de contraseña esta vacio")
     } else if (nombreus.value.length > 25) {
         alert("USUARIO SUPERA EL LIMITE DE CARACTERES")
     }
-    if (correous.value.length > 30) {
+    if (pasword.value.length > 30) {
         alert("LA CONTRASEÑA SUPERA EL LIMITE DE CARACTERES")
     }
-
 })
 
 let buttonlim = document.getElementById('btn-limpiar');// busca el boton limpiar
 buttonlim.addEventListener('click', function () {
     let nombreus = document.getElementById('nombreus');
     nombreus.value = '';
-    let correous = document.getElementById('pasword');
-    correous.value = '';
+    let pasword = document.getElementById('pasword');
+    pasword.value = '';
     alert('Datos limpiados')
 })

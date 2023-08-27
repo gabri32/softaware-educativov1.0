4b28aaa7eda3cd7694674758e3f463ad53b3c612
gabri32 <88991735+gabri32@users.noreply.github.com>
2023-08-27 11:46:31 -0500

    cambios loginfuncional

---
+++ datos.js
@@ -0,0 +1,66 @@
+var datosnom = ["gabriel"];
+var datoscorreo = ["gabodelac@gmail.com"];
+var usuariounic = ["admin gab"];
+var pasword = ["1234"];
+
+function agregarNombre(nuevoNombre, nuevocorreo, nuevousuario, newpasword) {
+    datosnom.push(nuevoNombre);
+    datoscorreo.push(nuevocorreo);
+    usuariounic.push(nuevousuario);
+    pasword.push(newpasword);
+}
+
+document.getElementById("registrobs").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    const nuevoNombre = document.getElementById("nomregis").value;
+    const nuevocorreo = document.getElementById("correoregis").value;
+    const nuevousuario = document.getElementById("usnew").value;
+    const newpasword = document.getElementById("newpas").value;
+
+    agregarNombre(nuevoNombre, nuevocorreo, nuevousuario, newpasword);
+
+    
+    document.getElementById("nomregis").value = "";
+    document.getElementById("correoregis").value = "";
+    document.getElementById("usnew").value = "";
+    document.getElementById("newpas").value = "";
+
+   
+    console.log("datosnom actualizado:", datosnom);
+});
+
+document.getElementById("identrar").addEventListener("click", function (event) {
+    event.preventDefault();
+
+    var correoIngresado = document.getElementById("correo").value;
+    var contraseñaIngresada = document.getElementById("contraseña").value;
+
+   
+    var correoEncontrado = false;
+    for (var i = 0; i < datoscorreo.length; i++) {
+        if (correoIngresado === datoscorreo[i]) {
+            correoEncontrado = true;
+            break; 
+        }
+    }
+
+    
+    if (correoEncontrado) {
+        var contraseñaCorrespondiente = pasword[i]; 
+        if (contraseñaIngresada === contraseñaCorrespondiente) {
+            alert("Inicio de sesión exitoso");
+        } else {
+            alert("Contraseña incorrecta");
+        }
+    } else {
+        alert("El correo no se encuentra registrado");
+    }
+});
+
+
+
+
+
+
+
